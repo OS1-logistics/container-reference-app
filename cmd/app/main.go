3ac294f61b744f0bddf9baa8c50e19dc9a1f2d93
Vinoth Kumar Shanmugam <vinoth.shanmugam@delhivery.com>
2023-01-03 14:14:53 +0530

    update package names, add aaa spec

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang/glog"
 	api "github.com/os1-logistics/container-reference-app/api/v1"
 )
 
+var (
+	env = os.Getenv("ENVIRONMENT")
+)
+
 func main() {
 	glog.Info("Starting Server")
 	g := gin.Default()
+
+	g.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "ok"})
+	})
+
 	s := api.NewServerV1()
 	api.RegisterHandlers(g, s)
+
 	g.Run(fmt.Sprintf(":%d", 3000))
 }
